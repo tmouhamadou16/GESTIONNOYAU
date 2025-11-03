d127dab53070fdcf633cdd0de377375774bf13c5
tmouhamadou16@gmail.com <tmouhamadou16@gmail.com>
2025-11-03 00:37:30 +0000

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { PagesComponent } from './pages/pages.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { DashboardComponent } from './pages/dashboard.component';
+import { ProduitsComponent } from './pages/produits/produits.component';
+import { StocksComponent } from './pages/stocks/stocks.component';
+import { UsersComponent } from './pages/users/users.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopageFoundComponent,
+    PagesComponent,
+    ClientsComponent,
+    DashboardComponent,
+    ProduitsComponent,
+    StocksComponent,
+    UsersComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nopage-found/nopage-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopage-found',
+  templateUrl: './nopage-found.component.html',
+  styleUrls: ['./nopage-found.component.css']
+})
+export class NopageFoundComponent {
+
+}
